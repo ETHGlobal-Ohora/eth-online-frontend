2f0253c51db2ba825edd16d05f93467ed1529fc8
Max <idhdb1004@naver.com>
2024-09-07 02:59:15 +0900

    Updated

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import NFT from "./pages/NFT";
-import Login from "./app/login/index.jsx";
-import Profile from "./app/profile/index.jsx";
-import ConnectCode from "./app/connect_code/index.jsx";
-import Connected from "./app/connected/index.jsx";
+import Login from "./app/login/index";
+import Profile from "./app/profile/index";
+import ConnectCode from "./app/connect_code/index";
+import Connected from "./app/connected/index";
 import React from "react";
 
 export const AppRouter = () => {
